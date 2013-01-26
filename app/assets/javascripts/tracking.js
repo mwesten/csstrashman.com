aacf13d3b67f912f17c412b1492f06804aa80772
Joe Nelson <cred+github@begriffs.com>
2013-01-26 14:46:32 -0600

    Add custom analytics goal tracking

---
+++ app/assets/javascripts/tracking.js
@@ -0,0 +1,24 @@
+$(function() {
+  var wait_time = 0;
+
+  if(_gaq) { // analytics is only initialized in production
+    $('span.stButton').click(function() {
+      _gaq.push(['_trackEvent', 'social', 'click']);
+    });
+    $('form.url').submit(function() {
+      _gaq.push(['_trackEvent', 'query', 'submit']);
+    });
+    if($('code.language-css').length) {
+      _gaq.push(['_trackEvent', 'result', 'view']);
+    }
+    if($('div.loading').length) {
+      window.setInterval(
+        function() {
+          wait_time += 2;
+          _gaq.push(['_trackEvent', 'result', 'loading', 'wait_time', wait_time]);
+        },
+        2000
+      );
+    }
+  }
+});
